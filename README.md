e44dffd9a97e182c54ad8dfa08c22686688e325c
spidermanja <38@teragrep.com>
2022-02-07 16:37:52 +0200

    Init commit for setup

---
+++ README.md
@@ -0,0 +1,7 @@
+![Teragrep Logo](https://avatars.githubusercontent.com/u/71876378?s=200&v=4)
+
+# Syslog Javascript Client
+
+## RLO_08
+
+Client Library written in Javascript to send messages to a Syslog server
---
+++ src/test/js/test-main.js
@@ -0,0 +1,8 @@
+// Simple test
+describe('Array', () => {
+    describe('#indexOf()', () => {
+      it('should return -1 when the value is not present', () => {
+      expect(-1, [1,2,3].indexOf(4));
+      });
+    });
+  });
