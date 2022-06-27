c6869928982e9229f35384bc963cc26c82c4de0e
spidermanja <38@teragrep.com>
2022-06-27 12:32:12 +0300

    Update Test cases for the concurrent execution & metrics

--- src/test/js/test-TC-IT-Perf-02-8000msg.js
+++ src/test/js/test-TC-IT-Perf-02-8000msg.js
@@ -1,83 +1,8 @@
 /**
  * @todo:
- * 1- Load
- * Speed, RT, stability, resource usage like memory utilization, data transfer velocity, network bandwidith usage. 
- * There are some possible performance boolenecks. related to #ISS-10,  incosistencies or like  memory utilization issue.
- * Matrix of the Memory allocation, CPU, network 
- * Note that The version of code that runs correctly one day might leak memory in the future due to a change in load, 
- * a new integration, or a change in the environment in which the application is run.
- * 
- * NOTE:
- * 1 - 'this' will also become a global variable as arrow functions, need to be mindful not to create accidental globals
- * 2 - destructure objects and use only the fields you need from an object or array rather than passing around entire objects 
- *     or arrays to functions, closures, timers, and event handlers.
- * 3 - Closures, event handlers, timers can mostly lead to memory leaks, thus properly takes care them.
- * 
- * 4 - Memwatch, 
- * 
- *  ⚠️⚠️⚠️ @Comment - Fix on the RLP_02 RelpConnection Line 485 required, thus this need to update RLP_02 the package on NPM Registry
- * 
- *   
- *   *****
- * 
- * GC Metrics:
- * Time consumption 
- * Tracing ; node --trace-gc test-TC-IT-Perf-02.js 
- * install clinic
- * 
- * Build test case around to potential data transfer rate & network bandwidith RELP Server
- * Objective: Obtain the average speed of data being sent to the RELP Server on the other end over the last few seconds, is this possible??
- * data transfer rate, 
- * 🔬 Possible integration of the NodeJS Inbuilt mechanism - PerfHooks 
- *  1 - Env config include HW, SW, Network
- *  2 - Processor & Memory Usage - Done
- *    - Applicalbe Disk time, bandwidth, private bytes, 
- * 
- * 
- * Update the documentation with resource usage metrics & insights. According to my observation, not a memory leak.
- * couldn't properly configure the perfomance hooks yet. 
- * now monitoring the network traffic metrics using tcpdump utility. 
- * hope to demo the findings and hear the comments 
- */
-
-/*
- const SyslogMessage = require('../../main/js/SyslogMessage');
- const Severity = require('../../main/js/Severity')
- const Facility = require('../../main/js/Facility')
- const SDElement = require('../../main/js/SDElement')
- const SDParam = require('../../main/js/SDParam');
- 
- 
- 
- const dateTimestamp = '2014-07-24T17:57:36+03:00';
- const dateTimestamp2 = '2020-07-30T17:57:36+03:00'; // Supply the timestamp with valid date format
- const dateFormat = new Date(dateTimestamp2);
- const timestamp = (new Date(dateTimestamp)).getTime();
- 
- let message = new SyslogMessage.Builder()
-         .withAppName('bulk-data-sorted') //valid
-         .withTimestamp(timestamp) // In case if the timestamp disabled, it will go with system timestamp.
-         .withHostname('iris.teragrep.com') //valid
-        
-         .withFacility(Facility.LOCAL0)
-         .withSeverity(Severity.INFORMATIONAL)
-         .withProcId('8740') //validatied for the PRINTUSASCII format
-         .withMsgId('ID47')
-         .withMsg('Todays lucky number is 17649276') // Fixed
-         .withSDElement(new SDElement("exampleSDID@32473", new SDParam("iut", "3"), new SDParam("eventSource", "Application"))) // Fix the space before the previous 
-         .build()
- 
-  
- 
- let rfc5424message;
- 
-  async function load() {
-     rfc5424message = await message.toRfc5424SyslogMessage();
-     console.log(rfc5424message.toString());
- }
- 
- load(); 
- 
+ * Monitor the network traffic using tcpdump with perlscript
+ * Generate further possible metrics,
+ * Update the findings on the confluence
 */
 
 var config = require('dotenv');
@@ -218,7 +143,10 @@ async function load() {
     dataPacket3 = await message3.toRfc5424SyslogMessage();
     dataPacket4 = await message4.toRfc5424SyslogMessage();
 }
-
+// Configure the console.log via the envir
+// Tune; SET Debug allow to programmatically setting the debug
+// Dynamic generation of batch size of 1 --> Prediction of RBT size handling 
+// Reverse Keyword, Bloom Filtering -- Bitfunnel, Bing   
 //Generate random number between 1 to 4
 function generateRandomNumber(max){
     return Math.floor(Math.random()* max) + 1;
@@ -247,11 +175,11 @@ function commit(){
 
         //OK, Let's increase the data load..... 
         // Max messages would be 9521, thus more than that could generate the BST(Binary Search Tree) lib's RangeError
-        for(let i = 0; i < 2500; i++){
+        for(let i = 0; i < 8000; i++){
             let packetNumber = generateRandomNumber(4)
             let dataPacket = 'dataPacket'+packetNumber;
             console.log('dataPacket'+packetNumber);
-            relpBatch.insert(dataPacket1);
+            relpBatch.insert(dataPacket2);
         }
 
 
